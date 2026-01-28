0abd03400280ce8f964d077325b3153e34b5267f
Milica Maksimović <milicam876@gmail.com>
2026-01-28 10:52:25 +0100

    Vezbe 6: animacije

---
+++ src/topic7_animation/Triangle.java
@@ -0,0 +1,49 @@
+package topic7_animation;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeLineJoin;
+import mars.drawingx.application.DrawingApplication;
+import mars.drawingx.drawing.Drawing;
+import mars.drawingx.drawing.DrawingUtils;
+import mars.drawingx.drawing.View;
+import mars.drawingx.gadgets.annotations.GadgetAnimation;
+import mars.drawingx.gadgets.annotations.GadgetDouble;
+import mars.drawingx.gadgets.annotations.GadgetVector;
+import mars.geometry.Vector;
+import mars.utils.Numeric;
+
+
+public class Triangle implements Drawing {
+
+    @GadgetAnimation
+    double time = 0.0;
+
+    @GadgetVector
+    Vector p0 = new Vector(-200, -200);
+
+    @GadgetVector
+    Vector p1 = new Vector(300, -100);
+
+    @GadgetVector
+    Vector p2 = new Vector(100, 200);
+
+    @GadgetDouble(min = 0, max = 1000)
+    double speed = 300;                                 // Brzina kretanja loptice (u pikselima po sekundi).
+
+
+
+    @Override
+    public void draw(View view) {
+        DrawingUtils.clear(view, Color.gray(0.125));
+
+        //TODO
+
+    }
+
+
+
+    public static void main(String[] args) {
+        DrawingApplication.launch(800, 800);
+    }
+
+}

66d31f82e35be653324b7443130e3921e3c505de
Milica Maksimović <milicam876@gmail.com>
2026-02-04 14:27:31 +0100

    Vezbe7: resenja

--- src/topic8_bezier_curves/Flower.java
+++ src/topic8_bezier_curves/Flower.java
@@ -52,8 +52,58 @@ public class Flower implements Drawing {
     public void draw(View view) {
         DrawingUtils.clear(view, Color.gray(0.125));
 
-        //TODO
+        // levi list
 
+        view.beginPath();
+        view.moveTo(o);
+        view.bezierCurveTo(ll1, ll2, l.add(Vector.polar(20, -0.36 * time)));
+        view.bezierCurveTo(lr1, lr2, o);
+
+        view.setFill(Color.hsb(110, 0.8, 0.6));
+        view.fill();
+
+        // stabljika
+
+        Vector c = s2.add(Vector.polar(30, 0.43 * time));
+
+        view.beginPath();
+        view.moveTo(o);
+        view.quadraticCurveTo(s1, c);
+
+        view.setLineWidth(12);
+        view.setStroke(Color.hsb(110, 0.8, 0.5));
+        view.stroke();
+
+        // desni list
+
+        view.beginPath();
+        view.moveTo(o);
+        view.bezierCurveTo(rl1, rl2, r.add(Vector.polar(20, 0.27 * time)));
+        view.bezierCurveTo(rr1, rr2, o);
+
+        view.setFill(Color.hsb(110, 0.8, 0.6));
+        view.fill();
+
+
+        // latice
+
+        for (int i = 0; i < nPetals; i++) {
+            Vector p0 = Vector.polar(rDisk         , (i + 0.0) / nPetals).add(c);
+            Vector p1 = Vector.polar(rDisk + lPetal, (i + 0.0) / nPetals).add(c);
+            Vector p2 = Vector.polar(rDisk + lPetal, (i + 1.0) / nPetals).add(c);
+            Vector p3 = Vector.polar(rDisk         , (i + 1.0) / nPetals).add(c);
+
+            view.beginPath();
+            view.moveTo(p0);
+            view.bezierCurveTo(p1, p2, p3);
+            view.setFill(Color.hsb(300, 0.3, 0.6));
+            view.fill();
+        }
+
+        // centar cveta
+
+        view.setFill(Color.hsb(50, 0.6, 0.9));
+        view.fillCircleCentered(c, rDisk);
     }
 
 
